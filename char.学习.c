41c0ef6d7cf1f7a6ec8e91548e03294c93b51151
QSDEL <137593187+QSDEL@users.noreply.github.com>
2023-09-20 22:09:56 +0800

    Add files via upload

---
+++ char.学习.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<math.h>
+void print()
+{
+	char a = 'C', b = 'H', c = 'I', d = 'N', e = 'A';
+	printf("%c%c%c%c%c\n", a, b, c, d, e);
+	printf("%c%c%c%c%c\n", e, d, c, b, a);
+
+}
+
+void printc()
+{
+	char c = 97, c1 = 98;
+	printf("%c %c", c, c1);
+	printf("%d %d", c, c1);
+}
+
+void printc1()
+{
+	printf("%c %c", 97 + 1, 'a' + 1);
+}
+
+void printc2()
+{
+	char a = 'a';
+	scanf_s("%c \n", &a);
+	if (a <= 97 && a >= 65)
+	{
+		printf("%c", a);
+	}
+	else
+		printf("%c", a -32);
+}
+
+void yuan()
+{
+	double r = 3.67, c, s, v;
+	c = 2 * 3.1415926 * r;
+	s = 3.1415926 * pow(r,2);
+	v = 4.0 / 3.0 * 3.1415926 *pow(r,3);
+	printf("c=%f,s=%f,v=%f\n",c,s,v);
+}
+
+void main()
+{
+	//print();
+	//printc();
+	//printc1();
+	/*printc2();*/
+	/*yuan();*/
+	float a = 2.0;
+	printf("%d", (int)a);    //强制类型转换
+}
+
+//简单的字符类型运用
