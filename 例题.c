6efeadeb0288e1031bc0eae3360ce0788231f8b6
QSDEL <137593187+QSDEL@users.noreply.github.com>
2023-09-27 11:25:37 +0800

    Add files via upload

---
+++ 例题.c
@@ -0,0 +1,35 @@
+//#include<stdio.h>
+//#include<math.h>
+//int main()
+//{
+//	double a, b, c, s, area;
+//	scanf_s("%1f,%1f,%1f", &a, &b, &c);
+//	s = (a + b + c) / 2.0;
+//
+//	area = sqrt(s * (s - a) * (s - b) * (s - c));
+//	printf("a=%f\nb=%f\nc=%f\narea=%f\n", a, b, c, area);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	char c1, c2;
+//	c1 = getchar();
+//	printf("%c,%d\n", c1, c1);
+//	c2 = c1 + 32;
+//	printf("%c,%d\n", c2, c2);
+//	return 0;
+//}
+//#include<stdio.h>
+//#include<math.h>
+//int main()
+//{
+//	double a, b, c, disc, x1, x2, p, q;
+//	scanf_s("a=%1f,b=%1f,c=%1f", &a, &b, &c);
+//	disc = b * b - 4 * a * c;
+//	p = -b / (2 * a);
+//	q = sqrt(disc) / (2 * a);
+//	x1 = p + q; x2 = p - q;
+//	printf("x1=%5.2f\nx2=%5.2f\n", x1, x2);
+//	return 0;
+//}
