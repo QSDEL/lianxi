d0b33970f291d2a07d7b69154da29040e57f2290
QSDEL <137593187+QSDEL@users.noreply.github.com>
2023-11-01 18:28:16 +0800

    Add files via upload

---
+++ ke-23-11-1.c
@@ -0,0 +1,35 @@
+//#include<stdio.h>
+
+//int* scan(int a[],int t) {
+//	for (int i = 0; i < t; i++)
+//	{
+//		*(a + i) = i;
+//	}
+//	return a;
+//}
+
+
+
+//int main()
+//{
+ //   int a[20] = {0};
+	//a[0] = 1;
+ //   a[1] = 1;
+	////scan(a,10);
+	////for (int i = 0; i < 10; i++)
+	////	printf("%d\n", *(a + i)=i);
+ //   for (int t = 0; t < 20; ++t)
+	// {
+	//	 if (t >= 2)
+	//		 a[t] = a[t - 1] + a[t - 2];
+	// }
+	//for (int i = 0; i < 20; i++)
+	//{
+	//	printf("%12d ", a[i]);
+	//	if ((i + 1) % 5 == 0)
+	//		printf("\n");
+	//}
+	//printf("%d", sizeof(a)/sizeof(a[0]));
+//	ace();
+//	return 0;
+//}
